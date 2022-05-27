2ca11ecc174404c8ae053e8ee7d6a23a6a564160
Max McNally <maxmcnally@gmail.com>
2022-05-27 10:55:34 -0700

    Time: 123 ms (12.12%), Space: 42.1 MB (54.98%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
@@ -2,9 +2,6 @@
  * @param {number[]} tickets
  * @param {number} k
  * @return {number}
-    customers = [1,2,0] 
-    tickets = [0]
-    k = 0
  */
 var timeRequiredToBuy = function(tickets, k) {
     let seconds = 0;
@@ -12,9 +9,8 @@ var timeRequiredToBuy = function(tickets, k) {
     while(tickets[k] > 0){
         if(tickets[pointer] !== 0){
             seconds++
-            tickets[pointer]-- 
+            tickets[pointer]--
         }
-        
         pointer++
         if(pointer > tickets.length - 1){
             pointer = 0;
