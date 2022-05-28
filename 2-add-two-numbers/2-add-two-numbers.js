b9d9799a4310a517a0f5e1a584c34cdd97e6aea0
Max McNally <maxmcnally@gmail.com>
2022-05-27 17:08:10 -0700

    Time: 259 ms (6.20%), Space: 51.3 MB (5.48%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val === undefined ? 0 : val)
+ *     this.next = (next === undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+var addTwoNumbers = function(l1, l2) {
+    let head = new ListNode(0)
+    let current = head
+    let carry = 0;
+    let node1 = l1
+    let node2 = l2
+    while(node1 || node2){ 
+        let value1 = node1 ? node1.val : 0;
+        let value2 = node2 ? node2.val : 0
+        node1 = node1 ? node1.next : null
+        node2 = node2 ? node2.next : null
+        let sum = value2 + value1 + carry
+        carry = sum >= 10 ? 1 : 0
+        sum = sum >= 10 ? sum % 10 : sum; 
+        console.log("Sum",sum)
+        current.next = new ListNode(sum)
+        current = current.next
+    }
+    let node = new ListNode()
+
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+    return head.next
+};
