3f190210ce7904eba65f223581182ff03744e401
Max McNally <maxmcnally@gmail.com>
2022-06-01 12:42:00 -0700

    Time: 115 ms (60.06%), Space: 48.3 MB (49.57%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+
+/*
+    for each row, jumps are [((N*2)-2) - 2 * row , (N*2)-2]
+    
+*/
+var convert = function(s, numRows) {
+    if(numRows >= s.length || numRows === 1){
+        return s
+    }
+    let result = ""
+    for(let i = 0; i < numRows; i++){
+        let jumps;
+        let baseJumps = (numRows*2)-2
+        if(i === 0 || i === numRows - 1){
+            jumps = [baseJumps]
+        }
+        else {
+            jumps = [baseJumps - 2 * i, baseJumps - (baseJumps - 2 * i)]
+        }
+        let start = i;
+        let jump = 0
+        while(s[start]){
+
+            result += s[start]
+            start += jumps[jump]
+            if(jumps[jump + 1]){
+                jump++
+            }
+            else {
+                jump = 0
+            }
+        }   
+    }    
+    return result
+};
