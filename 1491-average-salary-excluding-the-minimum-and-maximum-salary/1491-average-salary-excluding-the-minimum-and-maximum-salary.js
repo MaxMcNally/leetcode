82b23afeb449f13416f704845ded4d527b936bae
Max McNally <maxmcnally@gmail.com>
2022-06-02 13:01:49 -0700

    Time: 94 ms (23.39%), Space: 43.6 MB (5.46%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+  let min = Infinity
+  let max = -Infinity
+  let total = salary.length - 2
+  let sum = 0
+  for(let s of salary){
+      console.log(s)
+      if(s > max){
+          max = s
+      }
+      if(s < min){
+          min = s
+      }
+  }
+    console.log(max,min)
+    for(let s of salary){
+        if(s !== max && s !== min){
+            sum += s
+        }
+    }
+    return sum/total
+};
