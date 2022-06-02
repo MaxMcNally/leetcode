46578f251599ad1625ebe79678cf429fbdbc771f
Max McNally <maxmcnally@gmail.com>
2022-06-02 12:44:59 -0700

    Time: 73 ms (73.94%), Space: 41.8 MB (74.13%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,24 +3,10 @@
  * @return {number}
  */
 
-let memo = {}
 var fib = function(n) {
-    if(n === 1 || n === 0){
-        return n
+    let answer = [0,1]
+    for(let i = 2; i <= n; i++){
+        answer.push(answer[i - 2] + answer[i - 1])
     }
-    console.log(memo)
-    let one,two
-    if(memo[n - 1]){
-        one = memo[n - 1]
-    }
-    else {
-        memo[n - 1] = fib(n - 1)
-    }
-    if(memo[n - 2]){
-        two = memo[n - 2]
-    }
-    else {
-        memo[n - 2] = memo[n - 2]
-    }
-    return (memo[n - 1] || fib(n - 1) ) + (memo[n - 2] || fib(n - 2))
+    return answer[n]
 };
