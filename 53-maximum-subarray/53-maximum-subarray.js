ad2fc6e2fb17acb82599b17b2f9a55f7b1321b60
Max McNally <maxmcnally@gmail.com>
2022-06-02 16:03:47 -0700

    Time: 88 ms (84.00%), Space: 50.9 MB (17.60%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-        for (let i = 1; i < nums.length; i++){
+    for (let i = 1; i < nums.length; i++){
         nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
     }
     return Math.max(...nums);
