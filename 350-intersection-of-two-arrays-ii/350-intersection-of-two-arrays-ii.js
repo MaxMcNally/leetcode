101a7b9bad1c84d6c980f19919a483443645dac2
Max McNally <maxmcnally@gmail.com>
2022-06-03 08:05:21 -0700

    Time: 90 ms (48.83%), Space: 45.3 MB (9.18%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -5,16 +5,18 @@
  */
 var intersect = function(nums1, nums2) {
     let result = [];
+    let map = {}
     let longerArray = nums1.length > nums2.length ? nums1 : nums2;
     let shorterArray = nums1.length > nums2.length ? nums2 : nums1;
-    for(let i = 0; i < longerArray.length; i++){
-        if(shorterArray.indexOf(longerArray[i]) > -1){
-            result.push(longerArray[i]);
-            shorterArray.splice(shorterArray.indexOf(longerArray[i]), 1);
-            if(shorterArray.length === 0){
-                return result
-            }
-        }
+    for(let i = 0; i < shorterArray.length; i++){
+        map[shorterArray[i]] = map[shorterArray[i]] ? map[shorterArray[i]] + 1 : 1;
     }
+    for(let i = 0; i < longerArray.length; i++){
+        if(map[longerArray[i]] > 0){
+            result.push(longerArray[i])
+            map[longerArray[i]]--
+        }    
+    }
+    console.log(map)
     return result;
 };
