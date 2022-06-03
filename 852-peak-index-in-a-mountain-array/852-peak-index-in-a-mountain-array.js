ceb80d1d949fe75b5752be88baac434bd14c70b4
Max McNally <maxmcnally@gmail.com>
2022-06-02 21:24:38 -0700

    Time: 93 ms (27.70%), Space: 42.7 MB (51.24%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
@@ -3,16 +3,5 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    let highest = -1
-    let highestIndex = 0;
-    let counter = 0;
-    for(let peak of arr){
-        if(peak > highest){
-            highest = peak
-            highestIndex = counter
-        }
-        counter++
-    }
-    return highestIndex
-    
+    return arr.indexOf(Math.max(...arr))
 };
