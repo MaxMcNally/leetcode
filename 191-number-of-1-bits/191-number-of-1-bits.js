dcebd96175496a51a48d54a2f63066d61b827e19
Max McNally <maxmcnally@gmail.com>
2022-06-07 14:53:23 -0700

    Time: 64 ms (94.91%), Space: 42.5 MB (55.46%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -8,13 +8,10 @@
     
  */
 var hammingWeight = function(n) {
-    let bits = 0;
-    let mask = 1;
-    for(let i = 0; i < 32; i++){
-        if((n & mask) != 0){
-            bits++
-        }
-        mask <<= 1
+    let sum = 0
+    while(n !== 0){
+        sum++
+        n &= (n - 1)
     }
-    return bits
+    return sum
 };
