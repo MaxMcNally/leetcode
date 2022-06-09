15e9f2c42a4c9b7694c9ab0391d05531a5afb230
Max McNally <maxmcnally@gmail.com>
2022-06-08 17:37:03 -0700

    Time: 254 ms (17.66%), Space: 58.2 MB (7.17%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    //for each string
+    let output = []
+    let map = {}
+    for(let i = 0; i < strs.length; i++){
+        let word = strs[i]
+        let anagrams = [strs[i]]
+        let ordered = strs[i].split("").sort().join()
+        map[ordered] = map[ordered] ? map[ordered].concat([word]) : [word]
+    }
+    console.log(map)
+    return Object.values(map)
+};
