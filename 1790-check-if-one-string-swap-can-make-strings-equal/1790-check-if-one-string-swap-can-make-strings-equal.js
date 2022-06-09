ab0f65014a77b4dba0aaeb42b21adbf32436a467
Max McNally <maxmcnally@gmail.com>
2022-06-09 08:36:55 -0700

    Time: 105 ms (17.66%), Space: 42.2 MB (58.26%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var areAlmostEqual = function(s1, s2) {
+    if(s1 === s2) {
+        return true
+    }
+    if(s1.split("").sort().join() !== s2.split("").sort().join()){
+        return false
+    }
+    let count = 0;
+    for(let i = 0; i < s1.length; i++){
+        if(s1[i] !== s2[i]){
+            count++
+        }
+    }
+    return count === 2
+};
