d6bdfdabd901f35e6bda191e3173b2a267ccf40b
Max McNally <maxmcnally@gmail.com>
2022-06-14 10:20:37 -0700

    Time: 211 ms (35.30%), Space: 49.3 MB (15.71%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function romanToInt(s) {
+    const values = {
+        IV: 4,
+        IX : 9,
+        XL : 40,
+        XC : 90,
+        CD: 400,
+        CM : 900,
+        I : 1,
+        V  : 5,
+        X  : 10,
+        L  : 50,
+        C  : 100,
+        D  : 500,
+        M  : 1000
+    }
+    let result = 0;
+    for(let i = 0; i < s.length; i++){
+        if(s[i+1] && (values[s[i]+s[i+1]])){
+            result += values[s[i]+s[i+1]];
+            i++;
+        }
+        else if(values[s[i]]){
+            result += values[s[i]];
+        }
+    }
+    return result;
+    
+};
