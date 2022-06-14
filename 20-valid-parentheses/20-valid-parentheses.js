1e8db85ae67934d7ee6a54551d8a89bb12b22540
Max McNally <maxmcnally@gmail.com>
2022-06-14 12:02:50 -0700

    Time: 73 ms (70.62%), Space: 42 MB (82.31%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isValid = function(s) {
+    if(s.length < 2 || s.length % 2 > 0){
+        return false
+    }
+    let complements = {
+        "(" : ")",
+        "[" : "]",
+        "{" : "}"
+    }
+    const stack = [];
+    let counter = 0;
+    while(counter < s.length){
+        const letter = s.charAt(counter); //"["
+        if(complements[letter]){
+            stack.push(complements[letter]) //stack = [")"]
+        }
+        else {
+            const last = stack[stack.length - 1] //"]"
+            if(last && last === letter){
+                stack.pop()
+            }
+            else {
+                return false
+            }
+        }
+        counter++
+    }
+    return stack.length === 0
+};
