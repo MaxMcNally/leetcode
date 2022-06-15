883d1228007d6cf037f5b5b1774bf3359111d710
Max McNally <maxmcnally@gmail.com>
2022-06-15 12:45:21 -0700

    Time: 117 ms (20.33%), Space: 44.8 MB (35.56%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.js
+++ 101-symmetric-tree/101-symmetric-tree.js
@@ -10,39 +10,19 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+
 var isSymmetric = function(root) {
-    if(!root.left && !root.right) return true
-    if(!root.left || !root.right) return false
-    if(root.left.val !== root.right.val) return false
-    let leftStack = [root.left]
-    let rightStack = [root.right]
-    
-    while(leftStack.length > 0 || rightStack.length > 0){
-        if(leftStack.length !== rightStack.length){
-            return false
-        }
-        
-        let left = leftStack.pop()
-        let right = rightStack.pop()
+  return isSymmetricRecursive(root.left,root.right);
+}
 
-        if(right.val !== left.val) {
-          return false
-        }
-
-        if(left.left && right.right) {
-          leftStack.push(left.left)
-          rightStack.push(right.right)
-        }
-        else if(left.left || right.right){
-          return false
-        } 
-        if(left.right && right.left){
-            leftStack.push(left.right)
-            rightStack.push(right.left)
-        }
-        else if(left.right || right.left){
-          return false
-        }
-    }
+var isSymmetricRecursive = function (left,right){  
+  if(!left && !right){
     return true
-};
+  }
+  if(!left || !right){
+    return false
+  }
+  return left.val === right.val &&
+            isSymmetricRecursive(left.left,right.right) &&
+                isSymmetricRecursive(left.right,right.left);
+}
