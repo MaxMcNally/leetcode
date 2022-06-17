4f1a7cd0d0e8d120d1892d61809aa16cead866a5
Max McNally <maxmcnally@gmail.com>
2022-06-17 10:56:21 -0700

    Time: 62 ms (92.29%), Space: 42 MB (56.45%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -4,11 +4,15 @@
  */
 var numIdenticalPairs = function(nums) {
     let pairs = 0
+    let map = {}
     for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            if(nums[i] === nums[j]){
-                pairs++
-            }
+        if(map[nums[i]]){
+            
+            pairs += map[nums[i]]
+            map[nums[i]]++
+        }
+        else {
+            map[nums[i]] = 1
         }
     }
     return pairs
