7e3ada2af8d3d9ced40099131c819f8f9ff35bc1
Max McNally <maxmcnally@gmail.com>
2022-06-19 09:29:22 -0700

    Time: 154 ms (13.01%), Space: 47.7 MB (11.38%) - LeetHub

--- 1165-single-row-keyboard/1165-single-row-keyboard.js
+++ 1165-single-row-keyboard/1165-single-row-keyboard.js
@@ -7,11 +7,10 @@ var calculateTime = function(keyboard, word) {
     let moves = 0;
     let lastIndex = 0;
     for(let i = 0; i < word.length; i++){
-        //"cba"
-        let index = keyboard.split("").indexOf(word[i]) //0
-        let difference = Math.abs(lastIndex - index) //1 - 0 = 1
-        lastIndex = index //0
-        moves += difference //[2,1,1] 
+        let index = keyboard.split("").indexOf(word[i])
+        let difference = Math.abs(lastIndex - index)
+        lastIndex = index
+        moves += difference
     }
     return moves
 };
