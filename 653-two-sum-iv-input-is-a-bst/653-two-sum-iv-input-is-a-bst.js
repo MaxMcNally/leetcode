9ca4b0ddde55aff80cb360a2de91426d7fd078ab
Max McNally <maxmcnally@gmail.com>
2022-06-20 17:02:39 -0700

    Time: 127 ms (62.63%), Space: 52.7 MB (16.65%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+    
+    let map = {}
+    let queue = [root]
+    while(queue.length > 0){ //[[1]]
+        let node = queue.shift() //[1]
+        if(map[node.val]){ 
+            console.log("???")
+            return true
+        }
+        map[k - node.val] = true 
+        if(node.left) queue.push(node.left)
+        if(node.right) queue.push(node.right)
+    }
+    return false
+};
