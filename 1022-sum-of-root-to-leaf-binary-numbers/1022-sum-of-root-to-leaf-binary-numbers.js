ccd538fa5ab02d6b222cb8a5843d6cdd4d99a31d
Max McNally <maxmcnally@gmail.com>
2022-06-20 17:24:41 -0700

    Time: 65 ms (96.75%), Space: 44.7 MB (37.91%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumRootToLeaf = function(root) {
+    let paths = []
+    function search(node, path = ''){
+        if(!node.left && !node.right) paths.push(path + node.val)
+        if(node.left){ 
+            search(node.left, path + node.val)
+        }
+        if(node.right){ 
+            search(node.right, path + node.val)
+        }
+        
+    }
+    search(root)
+    console.log(paths)
+    return paths.reduce((memo,path)=>{
+        return memo + parseInt(path, 2) //from binary
+    },0)
+};
