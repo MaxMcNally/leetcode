6ae9060017137d72a6bd788967509efd87da30e8
Max McNally <maxmcnally@gmail.com>
2022-06-29 19:11:03 -0700

    Time: 94 ms (52.45%), Space: 44.2 MB (56.18%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.js
+++ 841-keys-and-rooms/841-keys-and-rooms.js
@@ -9,13 +9,10 @@ var canVisitAllRooms = function(rooms) {
     let q = [...rooms[0]]
     let currentRoom = rooms.length - 1
     while(q.length > 0){
-        console.log(q)
         let key = q.shift()
         if(visitedRooms[key] === undefined){
             visitedRooms[key] = true
             currentRoom--
-            console.log(visitedRooms)
-            console.log(currentRoom)
             q.push(...rooms[key])
         }
     }
