d1c65c1f7fad827b7d497d3549baf8d56c35650e
Max McNally <maxmcnally@gmail.com>
2022-07-02 16:26:06 -0700

    Time: 215 ms (5.19%), Space: 49.3 MB (7.04%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @return {TreeNode}
+ */
+
+var bstFromPreorder = function(preorder) {
+    let start = preorder.shift()
+    if(!start) return null
+    let root = new TreeNode(start)
+    let rightStart = preorder.findIndex((item) => item > start)
+    let right
+    let left
+    if(rightStart > -1){
+         right = preorder.slice(rightStart)
+         left = preorder.slice(0,rightStart)
+    }
+    else {
+        right = []
+        left = preorder
+    }
+    console.log(left,right)
+    if(left.length > 0){
+        root.left = bstFromPreorder(left)
+    }
+    if(right.length > 0){
+        root.right = bstFromPreorder(right)
+    }
+    return root
+};
