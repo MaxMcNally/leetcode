8300083b6dad4eab1c2a7667cc4494c787c56a8c
Max McNally <maxmcnally@gmail.com>
2022-07-07 18:27:24 -0700

    Time: 2036 ms (5.26%), Space: 45.5 MB (48.87%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} weights
+ * @param {number} days
+ * @return {number}
+ */
+
+function checkDays(w,d,weights){
+  let weight = 0
+  let numDays = 1
+  for(let i = 0; i < weights.length; i++){
+      weight += weights[i]
+      if(weights[i + 1] !== undefined && weight + weights[i + 1] > w){
+          numDays++
+          weight = 0
+      }
+  }
+  if(numDays <= d) return true
+  return false
+
+}
+
+var shipWithinDays = function(weights, days) {
+  
+  let minWeight = Math.max(...weights)
+  while(checkDays(minWeight, days, weights) === false){
+      minWeight++
+  }
+    return minWeight
+};
